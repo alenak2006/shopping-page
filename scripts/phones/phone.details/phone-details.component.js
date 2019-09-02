65809cb8145d7cc58b4726b1ffe14bf1945f3f1e
Alena Kaleda <alenak06@hotmail.com>
2019-09-02 18:14:00 +0100

    update

--- scripts/phones/phone.details/phone-details.component.js
+++ scripts/phones/phone.details/phone-details.component.js
@@ -1,17 +1,21 @@
 import { BaseComponent } from '../../shared/components/base.component.js'
 
 export class PhonesDetailsComponent extends BaseComponent {
+    hide() {
+        super.hide();
+    }
     show(phone) {
         this._phone = phone;
         this._render();
         super.show();
+
     }
     _render() {
         this._element.innerHTML = `
         <div>
         <img class="phone" src="${this._phone.imageUrl}">
 
-        <button>Back</button>
+        <button id = "back" >Back</button>
         <button>Add to basket</button>
         <h1>${this._phone.name}</h1>
         <p>${this._phone.snippet}</p>

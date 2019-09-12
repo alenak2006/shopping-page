e5d819ae3d6640947dc796ce951802a3744bcd3b
Alena Kaleda <alenak06@hotmail.com>
2019-09-12 14:42:19 +0100

    update

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 const nodeStatic = require('node-static');
 
-const file = new nodeStatic.Server(`${__dirname}/`);
+const file = new nodeStatic.Server(`${__dirname}/`,
+    {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Headers': '*'
+        }
+    });
 
 http.createServer((request, response) => {
     file.serve(request, response);
